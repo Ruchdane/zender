fcb9264a06bfd0a35a0ebd344b54066d114146e8
ruchdane <ruchdaneabiodun@gmail.com>
2022-06-15 14:42:10 +0100

    configuration de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,37 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/index.js'),
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, 'public/js'),
-        filename: 'app.js',
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /\/node_modules\//,
-            use: {
-                loader: 'babel-loader'
-            }
-        }, {
-            test: /\.(scss|css)$/,
-            use: ['style-loader', 'css-loader', 'sass-loader'],
-        }, ]
-    },
-    devServer: {
+	entry: path.resolve(__dirname, "src/index.js"),
+	mode: "development",
+	output: {
+		path: path.resolve(__dirname, "public/js"),
+		filename: "app.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /\/node_modules\//,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.(scss|css)$/,
+				use: ["style-loader", "css-loader", "sass-loader"]
+			}
+		]
+	},
+	devServer: {
         static: {
-          directory: path.join(__dirname, 'public'),
-        },
-        compress: true,
-        port: 3000,
-      },
-}
+            directory: path.join(__dirname, "public")
+		},
+        hot: true,
+        devMiddleware: {
+            publicPath: path.join(__dirname, "public"),
+            writeToDisk: true,
+         },    
+		compress: true,
+		port: 3000
+	}
+};
