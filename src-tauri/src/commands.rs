1cb1f95f873d50e539c50ab3d7423951cdc8702e
ruchdane <ruchdaneabiodun@gmail.com>
2022-06-16 20:02:42 +0100

    mise jour des api et fetures utiliser

---
+++ src-tauri/src/commands.rs
@@ -0,0 +1,18 @@
+use tauri::Manager;
+
+#[tauri::command]
+pub fn who_invoked_you(invoker: String) {
+  println!("I was invoked from {}!", invoker);
+}
+
+// Create the command:
+// This command must be async so that it doesn't run on the main thread.
+#[tauri::command]
+pub async fn close_splashscreen(window: tauri::Window) {
+  // Close splashscreen
+  if let Some(splashscreen) = window.get_window("splashscreen") {
+    splashscreen.close().unwrap();
+  }
+  // Show main window
+  window.get_window("main").unwrap().show().unwrap();
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,20 +1,33 @@
-use tauri::api::cli::get_matches;
+// use tauri::api::cli::get_matches;
+// use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
-#[tauri::command]
-fn my_custom_command() {
-  println!("I was invoked from JS!");
-}
+mod commands;
+mod file;
+mod tests;
 
 fn main() {
-  let context = tauri::generate_context!();
-  let cli_config = context.config().tauri.cli.clone().unwrap();
-  match get_matches(&cli_config,context.package_info()) {
-    Ok(matches) => {
-      println!("{:?}", matches)
-    }
-    Err(_) => {}
-  };
+  // let context = tauri::generate_context!();
+  // let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  // let close = CustomMenuItem::new("close".to_string(), "Close");
+  // let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+  // let menu = Menu::new()
+  //   .add_native_item(MenuItem::Copy)
+  //   .add_item(CustomMenuItem::new("hide", "Hide"))
+  //   .add_submenu(submenu);
+  // let cli_config = context.config().tauri.cli.clone().unwrap();
+  // match get_matches(&cli_config,context.package_info()) {
+  //   Ok(matches) => {
+  //     println!("{:?}", matches)
+  //   }
+  //   Err(_) => {}
+  // };
   tauri::Builder::default()
+    // .menu(menu)
+    .invoke_handler(tauri::generate_handler![
+      commands::who_invoked_you,
+      commands::close_splashscreen,
+      file::get_file_metadata
+    ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
