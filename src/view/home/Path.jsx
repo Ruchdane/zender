8a8e790d1a23c2ceaa2cbdbf7a7263c5eb12996a
Ruchdane <ruchdaneabiodun@gmail.com>
2022-08-20 16:16:03 +0100

    create test for home > path component

--- src/view/home/Path.jsx
+++ src/view/home/Path.jsx
@@ -2,7 +2,16 @@ import m from "mithril";
 import { Breadcrumb, BreadcrumbItem, Icon, Icons } from "construct-ui";
 import { path } from "@tauri-apps/api";
 
-
+/**
+ * Mithril Component
+ * 
+ * Show each element of a path 
+ * 
+ * Allow to go to any element of the path
+ * 
+ *  @param {string} path - The path to show
+ *  @param {(string) => {}} updatePath - The callback for when the path change
+ */
 export function Path() {
     return {
         view(vnode) {
@@ -14,21 +23,28 @@ export function Path() {
                         name: Icons.CHEVRON_RIGHT
                     })
             },
-                vnode.attrs.path.split(path.sep).map((segment, index, array) =>
-                    index !== array.length - 1
-                        ? m(BreadcrumbItem, {
-                            href: "#",
-                            class: "path-item",
-                            onclick: () => {
-                                let updatedPath = "";
-                                for (let i = 0; i <= index; i++) {
-                                    updatedPath += array[i] + path.sep;
+                vnode.attrs.path
+                    .split(path.sep)
+                    .filter((segment) => segment !== "")
+                    .map((segment, index, array) =>
+                        index + 1 !== array.length
+                            ? m(BreadcrumbItem, {
+                                href: "#",
+                                class: "path-item",
+                                onclick: () => {
+                                    let updatedPath = "";
+                                    for (let i = 0; i <= index; i++) {
+                                        updatedPath += array[i] + path.sep;
+                                    }
+                                    vnode.attrs.updatePath(updatedPath);
                                 }
-                                vnode.attrs.updatePath(updatedPath);
-                            }
-                        }, segment)
-                        : null)
-            );
+                            }, segment)
+                            : m(BreadcrumbItem, {
+                                class: "path-item",
+                            }, segment
+                            )
+                    )
+            )
         }
     };
 }
